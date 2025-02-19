7410ab5c3a39a5cb229f4cb8bfa35e3975b9696e
Zidna Billah Faza <zidnafaza27@gmail.com>
2025-02-19 15:19:12 +0700

    Controller - Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 
 /*
@@ -73,3 +74,15 @@ Route::get('/hello', [WelcomeController::class, 'hello']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticlesController::class, 'articles']);
+
+// Resource Controller
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
