bb65895f68919eb82a7f3e0bb269ef0607bfe0ca
Zidna Billah Faza <zidnafaza27@gmail.com>
2025-02-19 14:20:31 +0700

    Jobsheet 1

---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Jobsheet Penjelasan
+
+// Route::get('/hello', function () {
+//    return 'Hello World';
+// });
+
+// Route::get('/hello', [WelcomeController::class,'hello']);
+
+// Jobsheet Langkah-langkah
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return ('Selamat Datang');
+});
+
+Route::get('/about', function () {
+    return ('NIM    : 2341760030<br>Nama  : M. Zidna Billah Faza');
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+// Route Name
+
